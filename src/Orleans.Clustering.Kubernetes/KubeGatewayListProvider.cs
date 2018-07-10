0f2336270ad9cd0ba558225d96d2cc27cb777db9
Gutemberg Ribeiro <gutemberg@outlook.com>
2018-07-10 17:51:30 -0300

    Address #14 #13

--- src/Orleans.Clustering.Kubernetes/KubeGatewayListProvider.cs
+++ src/Orleans.Clustering.Kubernetes/KubeGatewayListProvider.cs
@@ -20,18 +20,17 @@ namespace Orleans.Clustering.Kubernetes
         private readonly KubeGatewayOptions _options;
         private readonly ILogger _logger;
         private readonly ILoggerFactory _loggerFactory;
-        private readonly TimeSpan _maxStaleness;
         private readonly string _clusterId;
 
         private KubeClient _kube;
 
-        public TimeSpan MaxStaleness => this._maxStaleness;
+        public TimeSpan MaxStaleness { get; private set; }
         public bool IsUpdatable => true;
 
         public KubeGatewayListProvider(ILoggerFactory loggerFactory, IOptions<KubeGatewayOptions> options, IOptions<ClusterOptions> clusterOptions, IOptions<GatewayOptions> gatewayOptions)
         {
             this._loggerFactory = loggerFactory;
-            this._maxStaleness = gatewayOptions.Value.GatewayListRefreshPeriod;
+            this.MaxStaleness = gatewayOptions.Value.GatewayListRefreshPeriod;
             this._logger = loggerFactory?.CreateLogger<KubeGatewayListProvider>();
             this._options = options.Value;
             this._clusterId = clusterOptions.Value.ClusterId;
@@ -43,7 +42,7 @@ namespace Orleans.Clustering.Kubernetes
             {
                 var silos = await this._kube.ListCustomObjects<SiloEntity>(PROVIDER_MODEL_VERSION, SiloEntity.PLURAL);
 
-                var gateways = silos.Where(s => s.Status == SiloStatus.Active && s.ProxyPort != 0)
+                var gateways = silos.Where(s => s.Status == SiloStatus.Active && s.ProxyPort != 0 && s.ClusterId == this._clusterId)
                     .Select(ConvertToGatewayUri).ToList();
                 return gateways;
             }
