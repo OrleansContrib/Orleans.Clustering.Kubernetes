0f36b234e28acec0c67c0c072ce866cf5df887c5
Gutemberg Ribeiro <gutemberg@outlook.com>
2020-06-11 03:08:39 -0300

    New client.

--- src/Orleans.Clustering.Kubernetes/Models/BaseEntity.cs
+++ src/Orleans.Clustering.Kubernetes/Models/BaseEntity.cs
@@ -1,9 +1,11 @@
+using Newtonsoft.Json;
 using Orleans.Clustering.Kubernetes.API;
 
 namespace Orleans.Clustering.Kubernetes.Models
 {
     internal class BaseEntity : CustomObject
     {
+        [JsonProperty("clusterId")]
         public string ClusterId { get; set; }
     }
 }
--- src/Orleans.Clustering.Kubernetes/Options/KubeGatewayOptions.cs
+++ src/Orleans.Clustering.Kubernetes/Options/KubeGatewayOptions.cs
@@ -2,9 +2,6 @@ namespace Orleans.Clustering.Kubernetes
 {
     public class KubeGatewayOptions
     {
-        public string Group { get; set; }
-        public string APIEndpoint { get; set; }
-        public string APIToken { get; set; }
-        public string CertificateData { get; set; }
+        public string Namespace { get; set; }
     }
 }
