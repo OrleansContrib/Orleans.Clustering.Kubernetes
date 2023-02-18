8c0e51d18b0b4df491d6036c6b9c072fa7f70379
Timm Hoffmeister <hoffmeistert@bluelizard.org.uk>
2023-02-18 11:25:36 +0000

    Log k8s server response for HttpOeprationException

---
+++ src/Orleans.Clustering.Kubernetes/Extensions/HttpOperationExceptionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using k8s.Autorest;
+
+namespace Orleans.Clustering.Kubernetes.Extensions;
+
+public static class HttpOperationExceptionExtensions
+{
+    public static string GetServerResponse(this HttpOperationException exception)
+    {
+        try
+        {
+            return exception.Response.Content;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
